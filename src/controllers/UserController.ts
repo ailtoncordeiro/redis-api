b5cb5d87f230e3a498cef5feee06f35bbb6c7801
Ailton Cordeiro <ailtoncordeiro@gmail.com>
2022-10-13 21:13:28 -0300

    first commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class UserController {
+
+    static async find(req: Request, res: Response) {
+        try {
+            console.time("Find Users");
+
+            const users = await prisma.user.findMany();
+
+            console.timeEnd("Find Users");
+            return res.json(users);
+        } catch (e) {
+            console.log(e);
+            return res.json({
+                error: e
+            })
+        }
+    }
+
+}
+
+export default UserController
